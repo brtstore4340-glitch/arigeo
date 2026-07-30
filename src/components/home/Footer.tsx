cfa09ef94650b7735a972053413db132d9444672
E0993599799 <ekkarat.mee@gmail.com>
2026-07-30 13:52:25 +0700

    refactor: simplify footer to show only colored social media icons with 'Follow us' title

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -1,20 +1,13 @@
 "use client";
 
-import { socialLinks, siteInfo } from "@/data/site-config";
+import { socialLinks } from "@/data/site-config";
 
 export default function Footer() {
   return (
     <footer className="arigeo-footer">
-      <div className="arigeo-footer-inner">
-        {/* Color Logo */}
-        <div className="footer-logo">
-          <a href="/">
-            <img src={siteInfo.logo} alt={siteInfo.name} />
-          </a>
-        </div>
-
-        {/* Social Media */}
-        <div className="footer-social">
+      <div className="footer-follow">
+        <h3 className="footer-follow-title">Follow us</h3>
+        <div className="footer-social-color">
           <a
             href={socialLinks.facebook}
             target="_blank"
@@ -23,7 +16,6 @@ export default function Footer() {
           >
             <img src="/images/social/facebook-color.png" alt="Facebook" />
           </a>
-
           <a
             href={socialLinks.instagram}
             target="_blank"
@@ -32,7 +24,6 @@ export default function Footer() {
           >
             <img src="/images/social/instagram-color.png" alt="Instagram" />
           </a>
-
           <a
             href={socialLinks.youtube}
             target="_blank"
@@ -41,7 +32,6 @@ export default function Footer() {
           >
             <img src="/images/social/youtube-color.png" alt="YouTube" />
           </a>
-
           <a
             href={socialLinks.tiktok}
             target="_blank"
